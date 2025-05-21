7ad029fae80022d9990c8fbcb055450516f07839
Ranjitha <ranjitharaksha777@gmail.com>
2025-05-21 20:51:37 +0530

    adding backend on git

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/students', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log("MongoDB connected"));
+
+const studentRoutes = require('./routes/studentRoutes');
+app.use('/api/students', studentRoutes);
+
+app.listen(5000, () => console.log("Server running on port 5000"));
