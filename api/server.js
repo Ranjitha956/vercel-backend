5a780a4eb0d70389caabc6a0f61963e85c779b9b
Ranjitha <ranjitharaksha777@gmail.com>
2025-05-21 23:57:01 +0530

    initial commit - added backend code

--- api/server.js
+++ api/server.js
@@ -15,3 +15,15 @@ const studentRoutes = require('../routes/studentRoutes');
 app.use('/api/students', studentRoutes);
 
 app.listen(5000, () => console.log("Server running on port 5000"));
+app.get("/", (req, res) => {
+  res.send("Backend is working!");
+});
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://admin:Admin123@cluster0.uepasp0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected successfully'))
+.catch(err => console.error('MongoDB connection error:', err));
+
